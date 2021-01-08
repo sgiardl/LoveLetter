59d179eb9dd022fc0e1f59cd60b95bf6c2bbc78f
Simon Giard-Leroux <70918466+sgiardl@users.noreply.github.com>
2021-01-08 00:37:59 -0500

    initial main and classes

--- classes.py
+++ classes.py
@@ -1,9 +1,73 @@
 # Love Letter
 
+import random as rd
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.deck = []
+
+    def draw(self, card):
+        self.deck.append(card)
+        
+    def __str__(self):
+        chars = []
+        
+        i = 1
+        
+        for card in self.deck:
+            chars.append(f'{i} : {card.char}')     
+            
+            i += 1
+        
+        return f'{chars}'
+    
+    def discard(self, index):
+        self.deck.pop(index)
+
 
 
 class Card:
     def __init__(self, char, strength):
         self.char = char
         self.strength = strength
+
+    def __str__(self):
+        return f'{self.char}'
+
+class Deck:
+    def __init__(self):
+        chars = ['Guard', 'Priest', 'Baron', 'Handmaid',
+                 'Prince', 'King', 'Countess', 'Princess']
         
+        strengths = [1, 2, 3, 4, 
+                     5, 6, 7, 8]
+        
+        numbers = [5, 2, 2, 2,
+                   2, 1, 1, 1]
+        
+        self.deck = []
+        
+        for i in range(len(chars)):
+            for j in range(numbers[i]):
+                self.deck.append(Card(chars[i], strengths[i]))
+                
+        rd.shuffle(self.deck)
+    
+    def deal(self):
+        return self.deck.pop()
+        
+    def __len__(self):
+        return len(self.deck)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- main.py
+++ main.py
@@ -1,1 +1,86 @@
 # Love Letter
+
+from classes import Deck, Player
+
+if __name__ == '__main__':
+#    while True:
+    print('\n')
+    print('*' * 40)
+    print('Jeu Love Letter')
+    print('*' * 40)
+    
+    nPlayers = 2 # int(input('Veuillez entrer le nombre de joueurs (2-4) : '))
+    
+    # List of players
+    players = []
+    
+    for i in range(nPlayers):
+        players.append(Player(f'Joueur {i + 1}')) # input(f'Nom joueur {i + 1} : ')))
+            
+    deck = Deck()
+    
+    # Initial draw for each player
+    for player in players:
+        player.draw(deck.deal())
+    
+    # One card is discarded in the beginning
+    discard = deck.deal()
+    
+    # Initialize to first turn
+    turn = 1
+            
+    while len(deck) > 0:
+        print(f'\n----- Tour {turn} -----')
+        
+        for player in players:
+            player.draw(deck.deal())
+            
+            print(f'\nMain de {player.name} : {player}')
+            choice = int(input('-> Quelle carte voulez-vous jouer? '))
+        
+            card = player.deck[choice - 1]
+        
+            if card.char == 'Guard':
+                
+                
+            # elif card.char == 'Priest':
+                
+                                    
+            # elif card.char == 'Baron':
+                 
+                           
+            # elif card.char == 'Handmaid':
+                
+                            
+            # elif card.char == 'Prince':
+                
+                                    
+            # elif card.char == 'King':
+                
+                                    
+            # elif card.char == 'Countess':
+                
+                                    
+            # elif card.char == 'Princess':
+            
+            player.discard(choice - 1)
+                 
+        
+        
+        
+        
+        
+        
+        
+            if len(deck) == 0:
+                break
+            
+        turn += 1
+            
+    print('==== Fin de la partie! Le gagnant est : ')
+
+    
+
+        # if input('Voulez-vous jouer une autre partie? (o/n) : ') != 'o':
+        #     break
+
